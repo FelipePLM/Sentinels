5dc3636faaf9a1cc6aab3c0c010091ceaf29f81f
Luis Felipe Pereira de Lima <luislima@plurismidia.com.br>
2025-05-28 09:16:36 -0300

    feat: Projeto iniciado

---
+++ src/main/java/com/br/plurismidia/easymonitor/EasymonitorApplication.java
@@ -0,0 +1,13 @@
+package com.br.plurismidia.easymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymonitorApplication.class, args);
+	}
+
+}
