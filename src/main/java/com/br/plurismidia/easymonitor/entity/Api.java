9b234b5b902aa2b5ab9921c3c98ece83286b3108
Luis Felipe Pereira de Lima <luislima@plurismidia.com.br>
2025-06-13 17:58:25 -0300

    feat: Corrigido entidade Api

--- src/main/java/com/br/plurismidia/easymonitor/entity/Api.java
+++ src/main/java/com/br/plurismidia/easymonitor/entity/Api.java
@@ -5,20 +5,20 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
-@Table(name = "CD_TB_API")
+@Table(name = "TB_API")
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
 public class Api {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "api_id")
+    @Column(name = "CD_API_ID")
     private Long id;
 
     @NotBlank(message = "O nome da API é obrigatório.")
-    @Column(name = "ds_nome")
+    @Column(name = "CD_DS_NOME")
     private String nome;
 
     @NotBlank(message = "A URL da API é obrigatória.")
-    @Column(name = "ds_url")
+    @Column(name = "CD_DS_URL")
     private String url;
 }
